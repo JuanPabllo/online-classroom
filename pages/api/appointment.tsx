6110224bd3e6617f690172bfd0e06ee5cfe82b74
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-04-27 16:44:17 -0300

    create appointmets POST/ appointment

---
+++ pages/api/appointment.tsx
@@ -0,0 +1,101 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ObjectID } from 'mongodb';
+import connect from '../../utils/database';
+
+interface ErrorResponseProp {
+  error: string;
+}
+
+interface SuccessResponseProp {
+  date: string;
+  teacher_name: string;
+  teacher_id: string;
+  student_name: string;
+  student_id: string;
+  course: string;
+  location: string;
+  appointment_link: string;
+}
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ErrorResponseProp | SuccessResponseProp>
+): Promise<void> => {
+  if (req.method === 'POST') {
+    const {
+      date,
+      teacher_name,
+      teacher_id,
+      student_name,
+      student_id,
+      course,
+      location,
+      appointment_link,
+    } = req.body;
+    if (
+      !date ||
+      !teacher_name ||
+      !teacher_id ||
+      !student_name ||
+      !student_id ||
+      !course ||
+      !location
+    ) {
+      res.status(404).json({ error: 'Missing parameter on request body' });
+      return;
+    }
+
+    const { db } = await connect();
+
+    const teacherExists = await db
+      .collection('users')
+      .findOne({ _id: new ObjectID(teacher_id) });
+
+    if (!teacherExists) {
+      res.status(404).json({
+        error: `Teacher ${teacher_name} with id ${teacher_id} does not exits`,
+      });
+      return;
+    }
+
+    const studentExists = await db
+      .collection('users')
+      .findOne({ _id: new ObjectID(student_id) });
+
+    if (!studentExists) {
+      res.status(404).json({
+        error: `Teacher ${student_name} with id ${student_id} does not exits`,
+      });
+      return;
+    }
+
+    const appointment = {
+      date,
+      teacher_name,
+      teacher_id,
+      student_name,
+      student_id,
+      course,
+      location,
+      appointment_link: appointment_link || '',
+    };
+
+    await db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectID(teacher_id) },
+        { $push: { appointment: appointment } }
+      );
+
+    await db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectID(student_id) },
+        { $push: { appointment: appointment } }
+      );
+
+    res.status(200).json(appointment);
+  } else {
+    res.status(400).json({ error: 'Wrong request method' });
+  }
+};
